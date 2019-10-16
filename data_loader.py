1a1ed6d3fcc8f413b11806d6017ac35ad7fd1730
klugh <klugh@student.ethz.ch>
2019-10-16 14:18:48 +0200

    implemented data_loader:  - loads data and reshapes it to 256*256
     added unet

--- data_loader.py
+++ data_loader.py
@@ -1,59 +1,67 @@
 import os
 import nibabel as nib
 from matplotlib import pyplot as plt
+import numpy as np
 
-data_dir = 'drlfom_bidsdata-1.0/'
+data_dir = '/Users/Hendrik/Documents/mlebe_data/mouse-brain-atlases/'
 
-im_data = []
 
-def visu1():
+def load_img():
+    img = []
+    im_data = []
     for o in os.listdir(data_dir):
-        if not o.startswith('.') and os.path.isdir(os.path.join(data_dir,o)):
-            for x in os.listdir(os.path.join(data_dir,o)):
-                if x == 'ses-ofM':
-                    for y in os.listdir(os.path.join(data_dir,o,x,'anat')):
-                        if y.endswith('.gz'):
-                            im_data.append(os.path.join(data_dir,o,x,'anat',y))
+        if o.endswith('200micron.nii'):
+            im_data.append(os.path.join(data_dir,o))
+
+    im_data = np.sort(im_data)
+    data = []
+    print(im_data)
+    for i in im_data:
+        img = nib.load(i)
+        img_data = img.get_data()
+        img_data = pad_img(img_data)
+        img_data = np.expand_dims(img_data,-1)
+        data.append(img_data)
+
+    # for i in range(data[0][1, 1].shape[0]):
+    #     plt.imshow(data[0][..., i], cmap='gray')
+    #     plt.savefig('visualisation/img_{}.pdf'.format(i))
+
+    return data
+
+
+def load_mask():
+    mask = []
+    im_data = []
+    for o in os.listdir(data_dir):
+        if o.endswith('200micron_mask.nii'):
+            im_data.append(os.path.join(data_dir,o))
 
     data = []
+    im_data = np.sort(im_data)
+    print(im_data)
+    for i in im_data:
+        img = nib.load(i)
+        img_data = img.get_data()
+        img_data = np.fliplr(img_data)          #todo masks are flipped?
+        img_data = pad_img(img_data)
+        img_data = np.expand_dims(img_data,-1)
+        data.append(img_data)
 
-    something = nib.load(im_data[0])
+    # for i in range(data[0][1, 1].shape[0]):
+    #     plt.imshow(data[0][..., i], cmap='gray')
+    #     plt.savefig('visualisation/mask_{}.pdf'.format(i))
 
-    for p, i in enumerate(im_data):
-        if p % 2 != 0:
-            img = nib.load(i)
-            img_data = img.get_data()
-            data.append(img_data)
-
-    for i in range(data[0][1,1].shape[0]):
-        plt.imshow(data[0][...,i], cmap='gray')
-        plt.show()
-
-
-def vis2():
-    for o in os.listdir(data_dir):
-        if not o.startswith('.') and os.path.isdir(os.path.join(data_dir, o)):
-            for x in os.listdir(os.path.join(data_dir, o)):
-                if x == 'ses-ofMcF2':
-                    for y in os.listdir(os.path.join(data_dir, o, x, 'anat')):
-                        if y.endswith('.gz'):
-                            im_data.append(os.path.join(data_dir, o, x, 'anat', y))
-
-    data = []
-
-    something = nib.load(im_data[0])
-
-    for p, i in enumerate(im_data):
-        if p % 2 != 0:
-            img = nib.load(i)
-            img_data = img.get_data()
-            data.append(img_data)
-
-    for i in range(data[0][1, 1].shape[0]):
-        plt.imshow(data[0][..., i], cmap='gray')
-        plt.show()
+    return data
 
 
-vis2()
-
-
+def pad_img(img):
+    shape = (256, 256)
+    padded = np.empty((shape[0],shape[1], img.shape[2]))
+    for i in range(img.shape[2]):
+        padd_y = shape[0] - img.shape[0]
+        padd_x = shape[1] - img.shape[1]
+        padded[...,i] = np.pad(img[..., i], ((padd_y//2, shape[0]-padd_y//2-img.shape[0]), (padd_x//2, shape[1]-padd_x//2-img.shape[1])),'constant')
+        # plt.imshow(padded[...,i], cmap='gray')
+        # plt.savefig('visualisation/padded/padded_{}.pdf'.format(i))
+    return padded
---
+++ network_trainer.py
@@ -0,0 +1,28 @@
+import data_loader as dl
+import model
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+import keras
+
+img_data = dl.load_img()
+mask_data = dl.load_mask()
+
+x_train = np.concatenate((img_data[0], img_data[1]), 2)
+x_train = np.reshape(x_train, (x_train.shape[2],x_train.shape[0],x_train.shape[1], x_train.shape[3]))
+y_train = np.concatenate((mask_data[0], mask_data[1]), 2)
+y_train = np.reshape(y_train, (y_train.shape[2],y_train.shape[0],y_train.shape[1], y_train.shape[3]))
+x_test = img_data[2]
+x_test = np.reshape(x_test, (x_test.shape[2],x_test.shape[0],x_test.shape[1], x_test.shape[3]))
+y_test = mask_data[2]
+y_test = np.reshape(y_test, (y_test.shape[2],y_test.shape[0],y_test.shape[1],y_test.shape[3]))
+
+model = model.unet()
+
+history = model.fit(x_train, y_train, epochs = 5)
+
+print(history.history.keys())
+
+model.save('test.h5')
+
+something = 0
