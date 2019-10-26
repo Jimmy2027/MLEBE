1b7d4cf8e58abe52c84d6c9b4f159d6e1e4cd0cc
klugh <klugh@student.ethz.ch>
2019-10-26 09:49:25 +0200

    trying preprocess bidsdata

--- bids_tester.py
+++ bids_tester.py
@@ -7,6 +7,7 @@ path = '/var/tmp/results/'
 
 for i in range(len(bids_datas)):
     input_image = bids_datas[i]
+    file_name = file_names[i]
     cmd = 'ResampleImage 3 {input} '.format(input=input_image) + path + '{output} 0.2x0.2x0.2'.format(output = file_names)
     os.system(cmd)
     print(cmd)
