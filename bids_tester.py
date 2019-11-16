9dc48b6b2e0d550debf450d0940393bbd78baf83
klugh <klugh@student.ethz.ch>
2019-11-16 23:15:23 +0100

    implemented bidstester callback

--- bids_tester.py
+++ bids_tester.py
@@ -1,4 +1,5 @@
 import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""   #predict on CPU instead of GPU
 import utils
 import nibabel as nib
 import numpy as np
@@ -9,81 +10,100 @@ from matplotlib import pyplot as plt
 
 import data_loader as dl
 
-# resample_save_path = '/var/tmp/resampled/'
-# utils.resample_bidsdata(resample_save_path)
-
-local = False
-test = True
-loss = 'bincross'
-shape = (128, 128)
-threshold = 0
-
-if local == True:
-    path = '/Users/Hendrik/Documents/mlebe_data/resampled/'
-    model_path = '/Users/Hendrik/Documents/Semester_project/results/unet_ep01_val_loss5.48.hdf5'
-    save_path = '/Users/Hendrik/Documents/mlebe_data/temp_bids/'
-else:
-    path = '/var/tmp/resampled/'
-    model_path = '/home/hendrik/src/mlebe/results/training_results/Dice_50/unet_ep05_val_loss0.04.hdf5'
-    save_path = '/home/hendrik/src/mlebe/results/bids_predictions_thr{}/'.format(threshold)
 
 
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
-data = []   #list of Nifti1Images
-for o in os.listdir(path):
-    if not o.startswith('.'):
-        print(o)
-        img = nib.load(os.path.join(path, o))
-        data.append(img)
+def bids_tester(save_path, model, remote, test, shape, epochs, threshold = 0):
+    if remote == False:
+        path = '/Users/Hendrik/Documents/mlebe_data/resampled/'
+    else:
+        path = '/var/tmp/resampled/'
+    save_path = save_path + '/bids_ep{epoch}/'.format(epoch = epochs)
 
-if loss == 'dice':
-    model = keras.models.load_model(model_path, custom_objects = {'dice_coef_loss': unet.dice_coef_loss})
-elif loss == 'bincross':
-    model = keras.models.load_model(model_path)
-else: print('wrong loss function defined')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
 
-y_pred = []
-img_datas = []
+    data = []  # list of Nifti1Images
+    for o in os.listdir(path):
+        if not o.startswith('.'):
+            img = nib.load(os.path.join(path, o))
+            data.append(img)
 
-if test:
-    data = data[:5]
+    y_pred = []
+    img_datas = []
 
-for i in data:
-    img_data = i.get_data()
-    img_data = utils.preprocess(img_data, shape)
-    i = np.expand_dims(img_data, -1)
-    temp = model.predict(i, verbose=1)
-    temp = np.where(temp > threshold, 1, 0)
-    y_pred.append(temp)
-    img_datas.append(img_data)
+    if test:
+        data = data[:5]
 
-temp = np.concatenate(y_pred, 0)
-plt.figure()
-plt.hist(np.unique(temp))
-plt.title('Histogram of the pixel values from the predicted masks')
-plt.savefig(os.path.join(save_path, 'hist.png'))
+    for i in data:
+        img_data = i.get_data()
+        img_data = utils.preprocess(img_data, shape)
+        i = np.expand_dims(img_data, -1)
+        temp = model.predict(i, verbose=1)
+        if threshold != 0:
+            temp = np.where(temp > threshold, 1, 0)
+        y_pred.append(temp)
+        img_datas.append(img_data)
 
-file_names = []
-for i in range(len(y_pred)):
-    x_test_affine = data[i].affine
-    x_test_header = data[i].header
-    file_name = os.path.basename(data[i].file_map['image'].filename)
-    file_names.append(file_name)
-    mask_temp = np.moveaxis(y_pred[i], 0, 2)
-    img_temp = np.moveaxis(img_datas[i], 0, 2)
-    # img = nib.Nifti1Image(temp, x_test_affine, x_test_header)
-    mask = nib.Nifti1Image(mask_temp, x_test_affine)
-    nib.save(mask, os.path.join(save_path, 'mask_' + file_name))
-    img = nib.Nifti1Image(img_temp, x_test_affine)
-    nib.save(img, os.path.join(save_path, 'resized_' + file_name))
+    temp = np.concatenate(y_pred, 0)
+    plt.figure()
+    plt.hist(np.unique(temp))
+    plt.title('Histogram of the pixel values from the predicted masks')
+    plt.savefig(os.path.join(save_path, 'hist.png'))
 
-output = []
-for i in range(len(data)):
-    output.append(np.squeeze(y_pred[i]))
+    file_names = []
+    for i in range(len(y_pred)):
+        x_test_affine = data[i].affine
+        x_test_header = data[i].header
+        file_name = os.path.basename(data[i].file_map['image'].filename)
+        file_names.append(file_name)
+        mask_temp = np.moveaxis(y_pred[i], 0, 2)
+        img_temp = np.moveaxis(img_datas[i], 0, 2)
+        # img = nib.Nifti1Image(temp, x_test_affine, x_test_header)
+        mask = nib.Nifti1Image(mask_temp, x_test_affine)
+        nib.save(mask, os.path.join(save_path, 'mask_' + file_name))
+        img = nib.Nifti1Image(img_temp, x_test_affine)
+        nib.save(img, os.path.join(save_path, 'resized_' + file_name))
 
-utils.save_datavisualisation2(img_datas, output, save_path, index_first=True, normalized= True, file_names=file_names)
+    output = []
+    for i in range(len(data)):
+        output.append(np.squeeze(y_pred[i]))
+
+    utils.save_datavisualisation2(img_datas, output, save_path, index_first=True, normalized= True, file_names=file_names)
 
 
+if __name__ == '__main__':
+
+    # resample_save_path = '/var/tmp/resampled/'
+    # utils.resample_bidsdata(resample_save_path)
+
+    remote = False
+    test = True
+    loss = 'bincross'
+    shape = (128, 128)
+    threshold = 0
+
+    if remote == False:
+        path = '/Users/Hendrik/Documents/mlebe_data/resampled/'
+        model_path = '/Users/Hendrik/Documents/Semester_project/results/unet_ep01_val_loss5.48.hdf5'
+        save_path = '/Users/Hendrik/Documents/mlebe_data/temp_bids/'
+    else:
+        path = '/var/tmp/resampled/'
+        model_path = '/home/hendrik/src/mlebe/results/training_results/Dice_50/unet_ep05_val_loss0.04.hdf5'
+        save_path = '/home/hendrik/src/mlebe/results/bids_predictions_thr{}/'.format(threshold)
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    data = []  # list of Nifti1Images
+    for o in os.listdir(path):
+        if not o.startswith('.'):
+            img = nib.load(os.path.join(path, o))
+            data.append(img)
+
+    if loss == 'dice':
+        model = keras.models.load_model(model_path, custom_objects={'dice_coef_loss': unet.dice_coef_loss})
+    elif loss == 'bincross':
+        model = keras.models.load_model(model_path)
+    else:
+        print('wrong loss function defined')
 
 
--- network_trainer.py
+++ network_trainer.py
@@ -1,8 +1,12 @@
 import data_loader as dl
+import utils
+import bids_tester
+
+
 import pickle
 import unet
-import utils
 import scoring_utils as su
+from tensorflow import keras
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from tensorflow.keras.optimizers import *
 import tensorflow.keras.preprocessing as kp
@@ -12,6 +16,8 @@ from matplotlib import pyplot as plt
 from sklearn import model_selection
 import nibabel as nib
 
+
+#todo parse arguments?
 #todo calculate dice score
 #todo write README
 #todo write scratches with useful functions
@@ -20,8 +26,8 @@ import nibabel as nib
 test = True
 remote = False
 visualisation = False  #if visualisation true saves pre- and unpreprocessed images for visualisation
-loss = 'bincross'
-epochs = 50
+losses = ['bincross', 'dice']
+epochs = 300
 seed = 1
 shape = (128, 128)
 
@@ -36,7 +42,7 @@ else:
     data_dir = '/Users/Hendrik/Documents/mlebe_data/mouse-brain-atlases/'  # local
 
 if test == True:
-    epochs = 1
+    epochs = 5
     save_dir = '/Users/Hendrik/Documents/mlebe_data/results/test/'
 else:
     save_dir = 'results/training_results/{loss}_{epochs}/'.format(loss = loss, epochs = epochs)
@@ -54,140 +60,152 @@ data_gen_args = dict(rotation_range=0.2,
 
 """shape = (z,y,x)"""
 
+for loss in losses:
 
-temp = dl.load_mask(data_dir)
-mask_data = []
+    temp = dl.load_mask(data_dir)
+    mask_data = []
 
-for i in range(len(img_data)):
-    mask_data.append(temp[0])
+    for i in range(len(img_data)):
+        mask_data.append(temp[0])
 
-print('*** splitting data into Train, Validation and Test set ***')
-if test == True:
-    x_train1_data, x_test_data , y_train1_data, y_test_data = model_selection.train_test_split(img_data, mask_data, random_state = seed, test_size=0.9)
-else:
-    x_train1_data, x_test_data, y_train1_data, y_test_data = model_selection.train_test_split(img_data, mask_data, random_state = seed, test_size=0.1)
-
-
-print('*** Preprocessing ***')
-x_train1 = utils.get_data(x_train1_data,shape, save_dir, visualisation = visualisation)[0]
-y_train1 = utils.get_data(y_train1_data,shape, save_dir, visualisation = visualisation)[0]
-x_test, x_test_affines, x_test_headers, file_names = utils.get_data(x_test_data, shape, save_dir)
-y_test, y_test_affines, y_test_headers = utils.get_data(y_test_data, shape, save_dir)[:3]  #todo ca fait aucun sens de preprocess 10 fois les memes masks
-
-print('*** saving Test data ***')
-x_test_struct = {
-    'x_test' : x_test,
-    'x_test_affines' : x_test_affines,
-    'x_test_headers' : x_test_headers,
-    'file_names' : file_names
-}
-
-y_test_struct = {
-    'y_test' : y_test,
-    'y_test_affines' : y_test_affines,
-    'y_test_headers' : y_test_headers,
-}
-
-xfile = open(save_dir + 'x_test_struct.pkl', 'wb')
-pickle.dump(x_test_struct, xfile)
-xfile.close()
-yfile = open(save_dir + 'y_test_struct.pkl', 'wb')
-pickle.dump(y_test_struct, yfile)
-yfile.close()
-
-x_train1 = np.concatenate(x_train1, axis = 0)
-y_train1 = np.concatenate(y_train1, axis = 0)
-x_train1 = np.expand_dims(x_train1, -1)
-y_train1 = np.expand_dims(y_train1, -1)
-x_train, x_val, y_train, y_val = model_selection.train_test_split(x_train1, y_train1, test_size=0.25)
+    print('*** splitting data into Train, Validation and Test set ***')
+    if test == True:
+        x_train1_data, x_test_data , y_train1_data, y_test_data = model_selection.train_test_split(img_data, mask_data, random_state = seed, test_size=0.9)
+    else:
+        x_train1_data, x_test_data, y_train1_data, y_test_data = model_selection.train_test_split(img_data, mask_data, random_state = seed, test_size=0.1)
 
 
-print('TRAINING SHAPE: ' + str(x_train.shape[1:4]))
-input_shape = (x_train.shape[1:4])
-model_checkpoint = ModelCheckpoint(save_dir + '/unet_ep{epoch:02d}_val_loss{val_loss:.2f}.hdf5', monitor='loss', verbose=1, save_best_only=True)
-if test == True:
-    model = unet.twolayernetwork(input_shape, 3, 0.5)
-    if loss == 'bincross':
-        print('Training with loss: binary_crossentropy')
-        model.compile(loss='binary_crossentropy',
-                      optimizer='adam',
-                      metrics=['accuracy'])
-    elif loss == 'dice':
-        print('Training with loss: dice-loss')
-        model.compile(loss = unet.dice_coef_loss,
-                      optimizer='adam',
-                      metrics=['accuracy'])
-    else: print('wrong loss function, choose between bincross or dice')
+    print('*** Preprocessing ***')
+    x_train1 = utils.get_data(x_train1_data,shape, save_dir, visualisation = visualisation)[0]
+    y_train1 = utils.get_data(y_train1_data,shape, save_dir, visualisation = visualisation)[0]
+    x_test, x_test_affines, x_test_headers, file_names = utils.get_data(x_test_data, shape, save_dir)
+    y_test, y_test_affines, y_test_headers = utils.get_data(y_test_data, shape, save_dir)[:3]  #todo ca fait aucun sens de preprocess 10 fois les memes masks
 
-else:
-    model = unet.unet(input_shape)
-    if loss == 'bincross':
-        print('Training with loss: binary_crossentropy')
-        model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
-    if loss == 'dice':
-        print('Training with loss: dice-loss')
-        model.compile(optimizer=Adam(lr=1e-4), loss=unet.dice_coef_loss, metrics=['accuracy'])
-    else: print('wrong loss function, choose between bincross or dice')
+    print('*** saving Test data ***')
+    x_test_struct = {
+        'x_test' : x_test,
+        'x_test_affines' : x_test_affines,
+        'x_test_headers' : x_test_headers,
+        'file_names' : file_names
+    }
 
-aug = kp.image.ImageDataGenerator(**data_gen_args)
+    y_test_struct = {
+        'y_test' : y_test,
+        'y_test_affines' : y_test_affines,
+        'y_test_headers' : y_test_headers,
+    }
+
+    xfile = open(save_dir + 'x_test_struct.pkl', 'wb')
+    pickle.dump(x_test_struct, xfile)
+    xfile.close()
+    yfile = open(save_dir + 'y_test_struct.pkl', 'wb')
+    pickle.dump(y_test_struct, yfile)
+    yfile.close()
+
+    x_train1 = np.concatenate(x_train1, axis = 0)
+    y_train1 = np.concatenate(y_train1, axis = 0)
+    x_train1 = np.expand_dims(x_train1, -1)
+    y_train1 = np.expand_dims(y_train1, -1)
+    x_train, x_val, y_train, y_val = model_selection.train_test_split(x_train1, y_train1, test_size=0.25)
 
 
-history = model.fit_generator(aug.flow(x_train, y_train), steps_per_epoch=len(x_train) / 32, validation_data=(x_val, y_val), epochs=epochs, verbose=1, callbacks=[model_checkpoint])
+    print('TRAINING SHAPE: ' + str(x_train.shape[1:4]))
+    input_shape = (x_train.shape[1:4])
+    model_checkpoint = ModelCheckpoint(save_dir + '/unet_ep{epoch:02d}_val_loss{val_loss:.2f}.hdf5', monitor='loss', verbose = 1, save_best_only=True, period = 10)
+
+    class bidstest(keras.callbacks.Callback):
+        def on_epoch_end(self, epoch, log = {}):
+            if epoch % 10 == 0:
+                bids_tester.bids_tester(save_dir, self.model, remote, test, shape, epoch)
+            return
+
+    bidstest_callback = bidstest()
+
+    if test == True:
+        model = unet.twolayernetwork(input_shape, 3, 0.5)
+        if loss == 'bincross':
+            print('Training with loss: binary_crossentropy')
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'])
+        elif loss == 'dice':
+            print('Training with loss: dice-loss')
+            model.compile(loss = unet.dice_coef_loss,
+                          optimizer='adam',
+                          metrics=['accuracy'])
+        else: print('wrong loss function, choose between bincross or dice')
+
+    else:
+        model = unet.unet(input_shape)
+        if loss == 'bincross':
+            print('Training with loss: binary_crossentropy')
+            model.compile(optimizer=Adam(lr=1e-4), loss='binary_crossentropy', metrics=['accuracy'])
+        if loss == 'dice':
+            print('Training with loss: dice-loss')
+            model.compile(optimizer=Adam(lr=1e-4), loss=unet.dice_coef_loss, metrics=['accuracy'])
+        else: print('wrong loss function, choose between bincross or dice')
+
+    aug = kp.image.ImageDataGenerator(**data_gen_args)
 
 
-
-print(history.history.keys())
-plt.figure()
-
-# Plot training & validation accuracy values:
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig(os.path.join(save_dir, 'accuracy_values.png'))
-plt.close()
-
-plt.figure()
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
-plt.savefig(os.path.join(save_dir, 'loss_values.png'))
-plt.close()
+    history = model.fit_generator(aug.flow(x_train, y_train), steps_per_epoch=len(x_train) / 32, validation_data=(x_val, y_val), epochs=epochs, verbose=1, callbacks=[model_checkpoint, bidstest_callback])
 
 
 
-y_pred = []
-dice_scores = []
-for i in x_test:
-    i = np.expand_dims(i, -1)
-    temp = model.predict(i, verbose=1)
-    y_pred.append(temp)
-    dice_scores.append(su.dice(i, temp))
+    print(history.history.keys())
+    plt.figure()
 
-dice_score = np.median(dice_scores)
-print('median Dice score: ', dice_score)
-file_names = []
-for i in range(len(y_pred)):
-    x_test_affine = x_test_data[i].affine
-    x_test_header = x_test_data[i].header
-    file_name = os.path.basename(x_test_data[i].file_map['image'].filename)
-    file_names.append(file_name)
-    temp = np.moveaxis(y_pred[i], 0, 2)
-    img = nib.Nifti1Image(temp, x_test_affine, x_test_header)
-    nib.save(img, os.path.join(save_dir, 'mask_' + file_name))
+    # Plot training & validation accuracy values:
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.savefig(os.path.join(save_dir, 'accuracy_values.png'))
+    plt.close()
 
-output = []
-for i in range(len(y_test)):
-    output.append(np.squeeze(y_pred[i]))
-
-utils.save_datavisualisation3(x_test, y_test, output, save_dir , index_first = True, normalized = True, file_names = file_names)
+    plt.figure()
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+    plt.savefig(os.path.join(save_dir, 'loss_values.png'))
+    plt.close()
 
 
-np.save(save_dir + 'y_pred_{}dice'.format(np.round(dice_score, 4)), y_pred)
+
+    y_pred = []
+    dice_scores = []
+    for i in x_test:
+        i = np.expand_dims(i, -1)
+        temp = model.predict(i, verbose=1)
+        y_pred.append(temp)
+        dice_scores.append(su.dice(i, temp))
+
+    dice_score = np.median(dice_scores)
+    print('median Dice score: ', dice_score)
+    file_names = []
+    for i in range(len(y_pred)):
+        x_test_affine = x_test_data[i].affine
+        x_test_header = x_test_data[i].header
+        file_name = os.path.basename(x_test_data[i].file_map['image'].filename)
+        file_names.append(file_name)
+        temp = np.moveaxis(y_pred[i], 0, 2)
+        img = nib.Nifti1Image(temp, x_test_affine, x_test_header)
+        nib.save(img, os.path.join(save_dir, 'mask_' + file_name))
+
+    output = []
+    for i in range(len(y_test)):
+        output.append(np.squeeze(y_pred[i]))
+
+    # bids_tester.bids_tester(save_dir, model, remote, epochs)
+
+    utils.save_datavisualisation3(x_test, y_test, output, save_dir , index_first = True, normalized = True, file_names = file_names)
+
+
+    np.save(save_dir + 'y_pred_{}dice'.format(np.round(dice_score, 4)), y_pred)
 
