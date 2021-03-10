bc5fd46f4d367c52d73243dabb98cff0b57f2e0f
klugh <klugh@student.ethz.ch>
2021-03-10 16:55:51 +0100

    cleanup

--- mlebe/masking/__init__.py
+++ mlebe/masking/__init__.py
@@ -1,18 +1,3 @@
-def get_masking_func(in_file,
-                     workflow_config_path,
-                     input_type='anat'):
-    from mlebe.training.utils.utils import json_file_to_pyobj
+# -*- coding: utf-8 -*-
 
-    workflow_config = json_file_to_pyobj(workflow_config_path).workflow_config
-    if workflow_config.model_type == '2D':
-        from mlebe.masking.predict_mask import predict_mask
-        return predict_mask(in_file,
-                            workflow_config_path,
-                            input_type=input_type)
-    elif workflow_config.model_type == '3D':
-        from mlebe.masking.predict_mask import predict_mask
-        return predict_mask(in_file,
-                            workflow_config_path,
-                            input_type=input_type)
-    else:
-        raise NotImplementedError('Model type [{}] is not implemented'.format(workflow_config.model_type))
+"""Masking scripts of the mlebe package."""
