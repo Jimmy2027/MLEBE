ef44b3a5d2f80d0f8c0833253cda0176d5a294e3
klugh <klugh@student.ethz.ch>
2021-03-10 16:55:44 +0100

    test masking with default models

--- mlebe/masking/tests/test_masking.py
+++ mlebe/masking/tests/test_masking.py
@@ -1,25 +1,31 @@
-import json
+# -*- coding: utf-8 -*-
 import os
-import shutil
+import tempfile
+from pathlib import Path
+from unittest import TestCase
 
 import nibabel as nib
 import numpy as np
-import tempfile
-import mlebe
+
 from mlebe.masking.predict_mask import predict_mask
 
 
-def test_masker():
-    with tempfile.TemporaryDirectory() as test_dir:
-        test_config = {"masking_config": {"masking_config_anat": {"test": True}}}
-        with open(os.path.join(test_dir, 'test_config.json'), 'w') as jsonfile:
-            json.dump(test_config, jsonfile, indent=4)
-        test_in_file = np.ones((63, 96, 48))
-        test_in_file = nib.Nifti1Image(test_in_file, np.eye(4))
-        test_in_file_dir = os.path.join(test_dir, 'test_in_file.nii.gz')
-        nib.save(test_in_file, test_in_file_dir)
-        _, _, _ = predict_mask(test_in_file_dir, os.path.join(test_dir, 'test_config.json'))
+class TestMlebeMasking(TestCase):
+    """Tests of the masking for the mlebe package."""
 
+    def test_masking(self):
+        """Test the masking function."""
+        with tempfile.TemporaryDirectory() as test_dir:
+            test_in_file = np.ones((63, 96, 48))
+            test_in_file = nib.Nifti1Image(test_in_file, np.eye(4))
+            test_in_file_path = Path(test_dir) / 'test_in_file.nii.gz'
+            nib.save(test_in_file, test_in_file_path)
+            for input_type in ['anat', 'func']:
+                nii_path_masked, [resampled_mask_path], resampled_mask_path = predict_mask(in_file=test_in_file_path,
+                                                                                           input_type=input_type)
 
-if __name__ == '__name__':
-    test_masker()
+                assert Path(
+                    nii_path_masked).exists(), f'nii_path_masked "{nii_path_masked}" does not exist, but should exist.'
+                assert Path(
+                    resampled_mask_path).exists(), f'resampled_mask_path "{resampled_mask_path}" does not exist, ' \
+                                                   f'but should exist.'
