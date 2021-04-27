427cb2cf0b34fbf8ab7bbac70cc201fee401bd20
klugh <klugh@student.ethz.ch>
2021-04-27 09:08:23 +0200

    clean up package setup

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
-from setuptools import setup, find_packages
+import setuptools
 
-requirements = [
-    'pandas',
-    'opencv-python',
-    'matplotlib',
-    'numpy',
-    'openpyxl',
-    'scikit-image',
-    'jsonschema',
-    'torch>=1.1',
-    'torchvision',
-    'tqdm',
-    'torchio',
-    'sklearn',
-    'xlrd'
-]
-
-setup(
-    name="MLEBE",
-    version="9999",
-    description="Machine Learning Enabled Brain Segmentation",
-    author="Hendrik Klug",
-    author_email="hendrik.klug@gmail.com",
-    url="https://github.com/Jimmy2027/MLEBE",
-    keywords=["fMRI", "data analysis", "deep learning"],
-    classifiers=[],
-    install_requires=requirements,
-    provides=["mlebe"],
-    packages=find_packages(include=['mlebe', 'mlebe.*']),
-    include_package_data=True,
-    data_files=['mlebe/training/results.csv'],
-    extras_require={},
-    test='pytest~=6.1'
-)
+if __name__ == '__main__':
+    setuptools.setup()
